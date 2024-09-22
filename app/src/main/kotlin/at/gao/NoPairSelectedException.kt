fedbcc306ac6a3cda69297d8ee077ca13314c12e
Simon Gao <simon.shubin@gmail.com>
2024-09-22 14:36:52 +0200

    💡 Added javadoc

--- app/src/main/kotlin/at/gao/NoPairSelectedException.kt
+++ app/src/main/kotlin/at/gao/NoPairSelectedException.kt
@@ -1,3 +1,8 @@
 package at.gao
 
+/**
+ * This exception gets thrown if no pair was selected for a wordtrainer.
+ * @author Simon Gao
+ * @version 2024-09-22
+ */
 class NoPairSelectedException : RuntimeException("There is no word+url pair selected!")
